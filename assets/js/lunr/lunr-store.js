2a8be493d46a680f23cdebcc0f8a4aa97bfb7b74
Boutroslab <m.boutros@boutroslab.org>
2019-06-16 12:55:13 +0200

    Update landing page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
+        "title": "HTSvis - a web app for exploratory data analysis ",
+        "excerpt":"HTSvis is an R/Shiny open-source web application for interactive visualization and exploratory analysis of data from arrayed high-throughput screens. The web application is either available via http://htsvis.dkfz.de/ or can be installed as an R package as described here. Shiny allows that the usage of the application in the default web...","categories": [],
+        "tags": ["Software","HTS","Web app"],
+        "url": "https://boutroslab.github.io/HTS-visualizer/",
+        "teaser":"https://boutroslab.github.io/assets/images/500x300-HTSvis.png"},{
         "title": "CRISPR Library Designer",
         "excerpt":"The CRISPR library designer (CLD) is an integrated software application for the design of single guide RNA (sgRNA) libraries for all organisms with annotated genomes. CLD is suitable for the design of libraries using modified CRISPR enzymes and targeting non-coding regions. The software is an end-to-end workflow for the fully...","categories": [],
         "tags": ["Software","CRISPR"],
         "url": "https://boutroslab.github.io/CRISPR-Library-Designer/",
-        "teaser":"https://boutroslab.github.io/assets/images/500x300.png"}]
+        "teaser":"https://boutroslab.github.io/assets/images/500x300-cld.png"}]
